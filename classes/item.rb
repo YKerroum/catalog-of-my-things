f33f965e07822ab4511e73bf225e9a4f8350dbf6
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-02-21 16:43:31 +0100

    fix linter errors

--- classes/item.rb
+++ classes/item.rb
@@ -1,34 +1,48 @@
+require 'date'
+require 'time'
+
 class Item
   private
+
   attr_accessor :id, :archived
 
   public
-  attr_accessor :genre, :author, :source, :label, :publish_date
 
-  def initialize(id, genre, author, source, label, publish_date, archived)
-    @id = id
+  attr_accessor :publish_date
+
+  def initialize(genre, author, label, publish_date = Time.now.to_d)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = publish_date
+    @archived = false
   end
 
   def genre=(genre)
     @genre = genre
+    genre.add_item(self)
   end
 
   def author=(author)
     @author = author
-  end
-
-  def source=(source)
-    @source = source
+    author.add_item(self)
   end
 
   def label=(label)
     @label = label
+    label.add_item(self)
   end
 
   private
+
   def can_be_archived?
     @archived = true
   end
 
-  
+  public
+
+  def move_to_archive
+    can_be_archived?
+  end
 end
