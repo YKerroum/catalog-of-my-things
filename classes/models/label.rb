4f25d3e0db5cd2cea9d8a1b0d2f6ec71c516d749
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-02-21 21:38:05 +0100

    add label properties

--- classes/models/label.rb
+++ classes/models/label.rb
@@ -1,6 +1,13 @@
 
 class Label
-  def initialize(name)
-    @name = name
+  private
+    attr_accessor :id, items
+  public
+    attr_accessor :title, :color
+  def initialize(title, color)
+    @id = rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
   end
 end
