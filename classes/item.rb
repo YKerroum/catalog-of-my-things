c1608e8ef5afd0957f83d4b48d62c53204d03656
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-02-21 16:29:08 +0100

    add contructor and customized setters of one to many properties to item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,5 +1,34 @@
 class Item
-  def initialize(id)
+  private
+  attr_accessor :id, :archived
+
+  public
+  attr_accessor :genre, :author, :source, :label, :publish_date
+
+  def initialize(id, genre, author, source, label, publish_date, archived)
     @id = id
   end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def source=(source)
+    @source = source
+  end
+
+  def label=(label)
+    @label = label
+  end
+
+  private
+  def can_be_archived?
+    @archived = true
+  end
+
+  
 end
