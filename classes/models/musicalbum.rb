e3b78f3c6a07f25214c45eab711004e31d3235cf
hopeforkoami <hopeforkoami@gmail.com>
2023-02-21 20:38:04 +0000

    Correct Linter errors

--- classes/models/musicalbum.rb
+++ classes/models/musicalbum.rb
@@ -1,6 +1,6 @@
 require_relative 'item'
 class MusicAlbum < Item
-  def initialize(genre, author, label, publish_date, on_spotify: false, id: nil)
+  def initialize(genre, author, label, publish_date, on_spotify: false, id: nil) # rubocop:disable Metrics/ParameterLists
     super(genre, author, label, publish_date, id)
     @on_spotify = on_spotify
   end
