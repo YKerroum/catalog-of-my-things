f69c3d931005be320dfd11914da5185c089b41cf
hopeforkoami <hopeforkoami@gmail.com>
2023-02-21 21:33:14 +0000

    Apply all review notes

--- classes/models/genre.rb
+++ classes/models/genre.rb
@@ -1,7 +1,9 @@
 class Genre
+  attr_reader :id
   attr_accessor :name, :items
 
-  def initialize(name)
+  def initialize(name, id: nil)
+    @id = id || Random.rand(1..1000)
     @name = name
     @items = []
   end
