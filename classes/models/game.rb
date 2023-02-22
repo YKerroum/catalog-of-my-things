c7f8dac2b6e90e1b3d5ff69a4ff49ed2186c0653
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-02-22 23:27:33 +0100

    add unit tests to Game class

--- classes/models/game.rb
+++ classes/models/game.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'date'
 
 class Game < Item
-  attr_accessor :platform, :multiplayer
+  attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played, publish_date, genre = nil, author = nil, label = nil, id = nil) # rubocop:disable Metrics/ParameterLists
     super(publish_date, genre, author, label, id)
@@ -19,21 +19,22 @@ class Game < Item
 
   public
 
-    def to_json(*args)
-        {
-            JSON.create_id => self.class.name,
-            'id' => @id,
-            'genre' => @genre,
-            'author' => @author,
-            'label' => @label,
-            'publish_date' => @publish_date,
-            'archived' => @archived,
-            'multiplayer' => @multiplayer,
-            'last_played_at' => @last_played_at
-        }.to_json(*args)
-    end
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'genre' => @genre,
+      'author' => @author,
+      'label' => @label,
+      'publish_date' => @publish_date,
+      'archived' => @archived,
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at
+    }.to_json(*args)
+  end
 
-    def self.json_create(object)
-        new(object['multiplayer'], object['last_played_at'], object['publish_date'], object['genre'], object['author'], object['label'], object['id'])
-    end
+  def self.json_create(object)
+    new(object['multiplayer'], object['last_played_at'], object['publish_date'], object['genre'], object['author'],
+        object['label'], object['id'])
+  end
 end
