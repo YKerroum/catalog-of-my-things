7882cf2bc84543f685c92e5e1f1cdf2f4e2fd988
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-02-22 02:08:00 +0100

    add options of List all books , List all labels and Add a book to the app

---
+++ classes/app/labels_manager.rb
@@ -0,0 +1,28 @@
+require_relative '../models/label'
+class LabelManager
+  def initialize
+    @labels = []
+  end
+
+  def add_label
+    puts 'Enter label title:'
+    title = gets.chomp
+    puts 'Enter label color:'
+    color = gets.chomp
+    label = Label.new(title, color)
+    @labels << label
+    label
+  end
+
+  def all_labels
+    if @labels.empty?
+      puts 'No labels in the catalog'
+    else
+      @labels.each do |label|
+        print "#{label.title} "
+      end
+      puts ''
+    end
+    @labels
+  end
+end
